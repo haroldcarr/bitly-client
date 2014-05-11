def29eb4cd71736e7dd12e13a78346eebb5b2539
Harold Carr <harold.carr@gmail.com>
2014-05-10 18:30:07 -0700

    Got linkLookup working.  The problem was the data expectation where not complete. Still need to figure out how to do shorten.

--- src/BitlyClientResponses.hs
+++ src/BitlyClientResponses.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Mar 03 (Mon) 20:39:50 by Harold Carr.
-Last Modified : 2014 Mar 21 (Fri) 16:33:51 by Harold Carr.
+Last Modified : 2014 May 10 (Sat) 17:34:30 by Harold Carr.
 -}
 
 {-# LANGUAGE FlexibleInstances #-}
@@ -43,9 +43,10 @@ data Response = ExpandResponse { er_short_url   :: Maybe String -- URI
                              , ir_error       :: Maybe String
                              , ir_title       :: Maybe String
                              }
-              | LinkLookupResponse { llr_aggregate_link :: String
+              | LinkLookupResponse { llr_error          :: Maybe String
+                                   , llr_aggregate_link :: Maybe String
                                    , llr_url            :: String
-                                     }
+                                   }
               | ShortenResponse { sr_new_hash    :: Integer
                                 , sr_url         :: String
                                 , sr_hash        :: String
@@ -57,17 +58,18 @@ data Response = ExpandResponse { er_short_url   :: Maybe String -- URI
     deriving (Eq, Show)
 
 instance FromJSON DataStatusCodeStatusTxt where
-    parseJSON = withObject "DataStatusCodeStatusTxt" $ \o ->
-        DSCST <$> o .: "data"
-              <*> o .: "status_code"
-              <*> o .: "status_txt"
+    parseJSON   = withObject "DataStatusCodeStatusTxt" $
+                  \o -> DSCST
+                        <$> o .: "data"
+                        <*> o .: "status_code"
+                        <*> o .: "status_txt"
 
 instance FromJSON ResponseData where
-    parseJSON = withObject "ResponseData" $ \o ->
-            ExpandResponseData     <$> o .: "expand"
-        <|> InfoResponseData       <$> o .: "info"
-        <|> LinkLookupResponseData <$> o .: "link_lookup"
---        <|> ShortenResponseData
+    parseJSON   = withObject "ResponseData" $
+                  \o ->     ExpandResponseData     <$> o .: "expand"
+                        <|> InfoResponseData       <$> o .: "info"
+                        <|> LinkLookupResponseData <$> o .: "link_lookup"
+                     -- <|> ShortenResponseData    <$> o .: ""
 
 instance FromJSON Response where
     parseJSON v =     expandParse     v
@@ -76,33 +78,42 @@ instance FromJSON Response where
                   <|> shortenParse    v
 
 expandParse :: Value -> Parser Response
-expandParse = withObject "expandParse" (\o -> ExpandResponse <$> o .:? "short_url"
-                                                             <*> o .:  "long_url"
-                                                             <*> o .:  "user_hash"
-                                                             <*> o .:  "global_hash"
-                                                             <*> o .:? "hash"
-                                                             <*> o .:? "error")
+expandParse     = withObject "expandParse" $
+                  \o -> ExpandResponse
+                        <$> o .:? "short_url"
+                        <*> o .:  "long_url"
+                        <*> o .:  "user_hash"
+                        <*> o .:  "global_hash"
+                        <*> o .:? "hash"
+                        <*> o .:? "error"
 
 infoParse :: Value -> Parser Response
-infoParse = withObject "infoParse" (\o ->  InfoResponse <$> o .:  "created_by"
-                                                        <*> o .:  "created_at"
-                                                        <*> o .:? "short_url"
-                                                        <*> o .:? "hash"
-                                                        <*> o .:? "user_hash"
-                                                        <*> o .:? "global_hash"
-                                                        <*> o .:? "error"
-                                                        <*> o .:? "title")
+infoParse       = withObject "infoParse" $
+                  \o -> InfoResponse
+                        <$> o .:  "created_by"
+                        <*> o .:  "created_at"
+                        <*> o .:? "short_url"
+                        <*> o .:? "hash"
+                        <*> o .:? "user_hash"
+                        <*> o .:? "global_hash"
+                        <*> o .:? "error"
+                        <*> o .:? "title"
 
 linkLookupParse :: Value -> Parser Response
-linkLookupParse = withObject "infoParse" (\o ->  LinkLookupResponse <$> o .: "aggregate_link"
-                                                                    <*> o .: "url")
+linkLookupParse = withObject "linkLookupParse" $
+                  \o -> LinkLookupResponse
+                        <$> o .:? "error"
+                        <*> o .:? "aggregate_link"
+                        <*> o .:  "url"
 
 shortenParse :: Value -> Parser Response
-shortenParse = withObject "shortenParse" (\o -> ShortenResponse <$> o .: "new_hash"
-                                                                <*> o .: "url"
-                                                                <*> o .: "hash"
-                                                                <*> o .: "global_hash"
-                                                                <*> o .: "long_url")
+shortenParse    = withObject "shortenParse" $
+                  \o -> ShortenResponse
+                        <$> o .: "new_hash"
+                        <*> o .: "url"
+                        <*> o .: "hash"
+                        <*> o .: "global_hash"
+                        <*> o .: "long_url"
 
 parseResponse :: String -> Either String DataStatusCodeStatusTxt
 parseResponse x = eitherDecode $ L.pack x
